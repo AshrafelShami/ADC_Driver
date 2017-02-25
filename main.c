8d52f75d6c5e1bbe1e5e5c010bcd5333a30718f6
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-02-25 18:04:17 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,9 +5,9 @@ int main(void){
 	DDRC |= 0xFF;
 	Struct_ADC ADC_Init_Struct;
 	ADC_Init_Struct.Vref_Reference_Select	=	Vref_VCC_With_ext_cap;
-	ADC_Init_Struct.Result_Adjustmment		=	Right_Adjustment;
-	ADC_Init_Struct.Prescaler_Select		=	Division_Factor_128;
-	ADC_Init_Struct.Trigger_Select			=	AutoTrigger_Disable;
+	ADC_Init_Struct.Result_Adjustmment	=	Right_Adjustment;
+	ADC_Init_Struct.Prescaler_Select	=	Division_Factor_128;
+	ADC_Init_Struct.Trigger_Select		=	AutoTrigger_Disable;
 	ADC_Init(ADC_Init_Struct);
 	
     while (1){
